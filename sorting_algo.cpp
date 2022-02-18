0f92f6b026fdfcbc3ffedb9bd5d81f2ccc550902
amdazm05 <amdali1998@yahoo.com>
2022-02-19 00:55:10 +0500

    added auto iterator

--- sorting_algo.cpp
+++ sorting_algo.cpp
@@ -14,5 +14,9 @@ Sorting<type>::Sorting( std::vector<type> &Vec)
 template <typename type>
 void Sorting<type>::bubble_sort( std::vector<type> &Vec)
 {
-    
+    for (auto it = begin (Vec); it != end (Vec); ++it) 
+    {
+        // The std::vector::data() is an STL in C++ which returns a direct pointer to the memory array used internally by the vector to store its owned elements
+        // it->at(1); //to test 
+    }
 }
